312c092341c1b25a084c445b459d078ae0b0933e
luisleitao181 <109726608+luisleitao181@users.noreply.github.com>
2024-04-18 18:21:09 +0100

    Update writenoncanonicalnew.c
    semana 18 abril new

--- writenoncanonical.c
+++ writenoncanonical.c
@@ -6,7 +6,7 @@
 #include <termios.h>
 #include <stdio.h>
 #include <stdlib.h>
-#define BAUDRATE B9600
+#define BAUDRATE B38400
 #define MODEMDEVICE "/dev/ttyS1"
 #define _POSIX_SOURCE 1 /* POSIX compliant source */
 #define FALSE 0
@@ -16,9 +16,9 @@ volatile int STOP=FALSE;
 
 int main(int argc, char** argv)
 {
-    int fd,c, res;
+    int fd,c, res,state=0,saiwhile=0;
     struct termios oldtio,newtio;
-    unsigned char buf[5];
+    unsigned char buf[7],rcvbuf[10];
     int i, sum = 0, speed = 0;
 
     if ( (argc < 2) ||
@@ -73,18 +73,86 @@ int main(int argc, char** argv)
 
 
     buf[0]=0x5c;
-    buf[1]=0x03;
-    buf[2]=0x01;
-    buf[3]=0x08;
-    buf[4]=0x5c;
+    buf[1]=0x38;
+    buf[2]=0x5c;
+    buf[3]=0x01;
+    buf[4]=0x07;
+    buf[5]=0x06;
+    buf[6]=0x5c;
 
-    for(int i=0;i<5;i++){
+    for(int i=0;i<(strlen(buf)-1);i++){
         printf("%02x\n",buf[i]);
     }
-    res = write(fd,buf,5);
-    printf("%d bytes written\n", res);
-    
+    res = write(fd,buf,strlen(buf));
+    printf("%d bytes written\n\n\n", res);
 
+    while(read(fd,rcvbuf,1)){
+        printf("%02x\n",rcvbuf[0]);
+        switch(state){
+                    case 0:
+                        if(rcvbuf[0]==0x5c){
+                            printf("flag RCV\n");
+                            state=1;
+                        }
+                        else{
+                            state=0;
+                        }   
+                        break;
+                    case 1:
+                        if(rcvbuf[0]==0x03){
+                            printf("A RCV\n");
+                            state=2;
+                        }
+                        else if(rcvbuf[0]==0x5c){
+                            printf("flag RCV (went back)\n");
+                            state=1;
+                        }
+                        else{
+                            state=0;
+                        }  
+                        break;
+                    case 2:
+                        if(rcvbuf[0]==0x06){
+                            printf("C RCV\n");
+                            state=3;
+                        }
+                        else if(rcvbuf[0]==0x5c){
+                            printf("flag RCV (went back)\n");
+                            state=1;
+                        } 
+                        else{
+                            state=0;
+                        } 
+                        break;
+                    case 3:
+                        if(rcvbuf[0]==0x05){
+                            printf("BCC RCV\n");
+                            state=4;
+                        }
+                        else if(rcvbuf[0]==0x5c){
+                            printf("flag RCV (went back)\n");
+                            state=1;
+                        }
+                        else{
+                            state=0;
+                        }  
+                        break;
+                    case 4:
+                        if(rcvbuf[0]==0x5c){
+                            printf("UA RCV\n");
+                            state=5;
+                        }
+                        else{
+                            state=0;
+                        }
+                        break;
+                }
+    if(state==5){
+    state=0;
+    break;
+}
+    
+    }
     /*
     O ciclo FOR e as instruções seguintes devem ser alterados de modo a respeitar
     o indicado no guião
