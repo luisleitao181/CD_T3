1f918e0456b85dcbf0b81cc0ff91fb29e6a436f9
luisleitao181 <109726608+luisleitao181@users.noreply.github.com>
2024-05-23 13:28:11 +0100

    Update linklayer.h

--- linklayer.h
+++ linklayer.h
@@ -11,10 +11,28 @@ typedef struct linkLayer{
 #define NOT_DEFINED -1
 #define TRANSMITTER 0
 #define RECEIVER 1
-
+#define MODEMDEVICE "/dev/ttyS1"
 #define Max_Size 250
 //SIZE of maximum acceptable payload; maximum number of bytes that application layer should send to link layer
 #define MAX_PAYLOAD_SIZE 1000
+
+//
+#define SET 0x07 
+#define UA 0x06
+#define DISC 0x0A
+#define FLAG 0x5C
+#define A_Tx 0x01 
+#define A_Rx 0x03 
+
+#define RR(n) (0x01 | (n<<4)) //000n0001
+#define REJ(n) (0x05 | (n<<4))//000n0101
+#define CONTROLFIELD_S(n) (0x08 | n<<6)////1n000000
+
+#define ESCAPE_OCTET 0x5D
+#define XOR_OCTET 0x20
+//
+
+
 
 //CONNECTION deafault values
 #define BAUDRATE_DEFAULT B38400
